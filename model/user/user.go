b0344d592aae1a5977057b3b2f0a4801e355207d
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-07-15 14:45:29 +0330

    [add] add some file and folder to project

--- model/user/user.go
+++ model/user/user.go
@@ -1,6 +1,12 @@
 package user
 
+import "fmt"
+
 type User struct {
 	Username string
 	Password string
+}
+
+func (u User) String() string {
+	return fmt.Sprintf("username: %s, password: %s\n", u.Username, u.Password)
 }
---
+++ pkg/file_readerr/file_r.go
@@ -0,0 +1,41 @@
+package filereaderr
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/yousefzinsazk78/test_2_web_app/model/user"
+)
+
+// CreateFile method create file if doesn't exists...
+func CreateFile(filename string) error {
+	file, err := os.Create("./stored_file/" + filename + ".txt")
+	if err != nil {
+		return fmt.Errorf("i can't create this file because \n%s", err)
+	}
+	file.Close()
+	return nil
+}
+
+func WriteData(filename string, user user.User) error {
+	file, err := os.OpenFile("./stored_file/"+filename+".txt", os.O_APPEND|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.WriteString(user.String())
+	if err != nil {
+		return err
+	}
+	fmt.Println("write operation completed!")
+	return nil
+}
+
+// ReadFile method read filename given and return data and error
+func ReadFile(filename string) (string, error) {
+	readFile, err := os.ReadFile("./stored_file/" + filename + ".txt")
+	if err != nil {
+		return "", err
+	}
+	return string(readFile), nil
+}
